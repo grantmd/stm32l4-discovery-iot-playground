87382283ba7fab55ea4c76309b0e1fe8ae00583b
Myles Grant <myles@mylesgrant.com>
2021-12-26 22:08:25 -0800

    Only init once, not in the loop

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,21 @@ use stm32l4::stm32l4x5;
 
 #[entry]
 fn main() -> ! {
+    // give us references to the peripherals
+    let peripherals = stm32l4x5::Peripherals::take().unwrap();
+    let rcc = &peripherals.RCC;
+    let gpiob = &peripherals.GPIOB;
+    let gpioe = &peripherals.GPIOE;
+
+    // Enable clocks for GPIOB and GPIOE
+    rcc.ahb2enr.write(|w| w.gpioben().set_bit());
+    rcc.ahb2enr.write(|w| w.gpioeen().set_bit());
+
+    // Set GPIO pins to output
+    gpiob.moder.modify(|_, w| w.moder2().output());
+    gpioe.moder.modify(|_, w| w.moder8().output());
+
     loop {
-        // give us references to the peripherals
-        let peripherals = stm32l4x5::Peripherals::take().unwrap();
-        let rcc = &peripherals.RCC;
-        let gpiob = &peripherals.GPIOB;
-        let gpioe = &peripherals.GPIOE;
-
-        // Enable clocks for GPIOB and GPIOE
-        rcc.ahb2enr.write(|w| w.gpioben().set_bit());
-        rcc.ahb2enr.write(|w| w.gpioeen().set_bit());
-
-        // Set GPIO pins to output
-        gpiob.moder.modify(|_, w| w.moder2().output());
-        gpioe.moder.modify(|_, w| w.moder8().output());
-
         // Set LEDs
         gpiob.odr.write(|w| w.odr2().set_bit()); // red LED on
         gpiob.odr.write(|w| w.odr2().clear_bit()); // red LED off
